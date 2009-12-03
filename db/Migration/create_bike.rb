b000c8adadd170b410f479a7c3c3033b63c7bbea
Atul <Admin@atulr.local>
2009-12-04 01:30:38 +0530

    Adding the migration for creating the bike table.changed the daatabase.yml to use the default db as mysql.

---
+++ db/Migration/create_bike.rb
@@ -0,0 +1,14 @@
+class CreateBikes < ActiveRecord::Migration
+  def self.up
+    create_table :bikes do |t|
+      t.column :bike_id, :string
+      t.column :brand, :string
+      t.column :cost, :integer
+      t.column :brand, :string
+    end
+  end
+
+  def self.down
+    drop_table :bikes
+  end
+end
