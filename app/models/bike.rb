be9dd98f7066ade02e24834d8e4ff9bf7f482d9d
sachin <sachuraju.yahoo.com>
2009-12-07 14:11:10 +0530

    added validations for brand and cost.

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,2 +1,11 @@
 class Bike < ActiveRecord::Base
+
+  validates_presence_of :brand
+  validates_numericality_of :cost
+  validate :cost_must_be_a_number
+
+  protected
+  def cost_must_be_a_number
+    errors.add(:cost, "should be at least 0.01") if cost.nil? || cost < 0.01
+  end
 end
--- db/migrate/20091205082122_remove_id_column_from_bike.rb
+++
@@ -1,9 +0,0 @@
-class RemoveIdColumnFromBike < ActiveRecord::Migration
-  def self.up
-    remove_column :bikes, :bike_id
-  end
-
-  def self.down
-    add_column :bikes, :bike_id
-  end
-end
